3d3f66c9acd15cff4c5f3d811b24a60b4f25b238
feliciaL3 <113386223+feliciaL3@users.noreply.github.com>
2023-11-13 18:54:54 +0200

    4

---
+++ lab4/src/domain/mediator/SchoolMediator.java
@@ -0,0 +1,15 @@
+//SchoolMediator.java
+package domain.mediator;
+
+import domain.observer.Student;
+
+public interface SchoolMediator {
+    void sendMessage(String message, Student student, Classroom room);
+    void addStudentToClassroom(Student student, Classroom room);
+    void removeStudentFromClassroom(Student student, Classroom room);
+    void addClassroom(Classroom room);
+}
+
+//The SchoolMediator interface declares methods for sending messages, adding students to a classroom, removing students from a classroom, and adding a classroom.
+//The Main class acts as the client that uses the mediator and colleagues.
+// It creates instances of the SchoolCoordinator (mediator), Classroom (colleague), and Student (observer) classes and demonstrates their interaction.
